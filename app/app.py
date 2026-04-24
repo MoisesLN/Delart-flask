5073b2fb4fd0055a85b1e5881954cb2a491a319b
Davi Dlleon Bandeira Silva <20241101110081@ifrn.local>
2026-04-24 16:12:52 -0300

    Feat: routes create, e incio da de read

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from db import db
+from flask import Flask, jsonify, request
+from db import db, Usuario, Agendamento
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -9,6 +9,28 @@ db.init_app(app)
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+#Função de Read (um endpoint pra ver todos, outro pra ver especifico)
+@app.route('/agendamentos', methods=['GET'])
+def ver_agendamentos():
+    ...
+
+@app.route('/agendamentos/<int:agendamento_id>', methods=['GET'])
+def ver_agendamento_especifico():
+    ...
+
+#Função Update
+@app.route('/criar-agendamento', methods=['POST'])
+def criar_agendar():
+    data = request.get_json()
+    novo_agendamento = {
+        "id": Agendamento.id,
+        "titulo": data.get('titulo'),
+        "descricao": data.get('descricao'),
+        "status": data.get('status')
+    }
+
+    return jsonify({"mensage": "Agendamento criado com sucesso", "agendamento": novo_agendamento}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/models.py
+++ app/models.py
@@ -4,3 +4,14 @@ class Usuario(db.Model):
     __tablename__ = 'usuarios'
 
     id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.__str__)
+    senha = db.Column(db.__str__)
+
+class Agendamento(db.Model):
+    __tablename__ = 'agendamento'
+
+    id = db.Column(db.Integer, primary_key=True)
+    agendamento_user = db.Column(db.__str__, db.ForeignKey('usuarios.id'))
+    titulo = db.Column(db.__str__)
+    descricao = db.Column(db.__str__)
+    status = db.Column(db.Integer)
