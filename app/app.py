9de92ff0c1a8924c1b76675170d0bb32b9a86cb1
Moises Lopes <moisesbruh5@gmail.com>
2026-04-24 16:15:17 -0300

    feat: página de cadastro

--- app/app.py
+++ app/app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db.init_app(app)
+app.secret_key = 'd626428838bab4dd0736eb2e79918e0e'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# db.init_app(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def cadastro():
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
